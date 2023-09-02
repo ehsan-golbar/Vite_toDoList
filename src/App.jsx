47435010bac5ad683f8904f35edff6788b4c4855
Ehsan <1382ehg@gmail.com>
2023-09-02 13:59:11 +0330

    make three div tags with responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./index.css";
 
 function App() {
-
-
   return (
-    <>
-        <h1 className='bg-slate-600'>Hello </h1>
-    </>
-  )
+    <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-3 ">
+      <div className="h-28 bg-blue-500">
+        <h1 className="bg-slate-600 text-center">Time </h1>
+        <div className="grid grid-cols-2">
+          <h1 className="bg-red-600 text-center">clock </h1>
+          <h1 className="bg-yellow-600 text-center"> tempreture </h1>
+          <h1 className="bg-gray-700 col-span-2 text-center">date of today</h1>
+          <h1 className="bg-red-400 col-span-2 text-center">Calender</h1>
+        </div>
+      </div>
+      <div className="h-28 bg-red-500">
+        <h1 className="bg-slate-600 text-center">Search </h1>
+        <div className="h--28 bg-yellow-400 text-center">
+          <h1 className="">Google</h1>
+          <h1>Icons Link</h1>
+        </div>
+      </div>
+      <div className="h-28 bg-purple-500 text-center lg:col-span-1 md:col-span-2">
+        <h1 className="bg-slate-600 ">To Do List </h1>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
